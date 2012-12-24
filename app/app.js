d9f0b6113d4f28836b6801b410c26500ce1949d3
Cheton Wu (RD-TW) <cheton_wu@trend.com.tw>
2012-12-24 03:25:36 -0800

    Make code snippets easier to read

--- app/app.js
+++ app/app.js
@@ -6,88 +6,98 @@ var fs = require('fs'),
     logger = require('./lib/logger'),
     settings = require('./config/settings');
 
+/**
+ * Auto-load bundled middleware
+ */
+var middleware = {};
+
+fs.readdirSync(__dirname + '/lib/middleware').forEach(function(filename) {
+    if ( ! /\.js$/.test(filename)) {
+        return;
+    }
+    var name = path.basename(filename, '.js');
+    middleware[name] = require('./lib/middleware/' + name);
+});
+
 module.exports = function() {
     // Main app
     var app = express();
 
-    var env = process.env.NODE_ENV || 'development';
-    if ('production' === env) {
-        app.use(express.errorHandler());
-        // a custom "verbose errors" setting which can be used in the templates via settings['verbose errors']
-        app.disable('verbose errors'); // disable verbose errors in production
-    }
-    if ('development' === env) {
-        app.use(express.errorHandler({
-            dumpExceptions: true,
-            showStack: true
-        }));
-
-        // a custom "verbose errors" setting which can be used in the templates via settings['verbose errors']
-        app.enable('verbose errors'); // enable verbose errors in development
-    }
-
+    /**
+     * Logger
+     */
     logger.init(settings.winston);
+    logger.registerAppHelper(app); // Register app.locals (app.helper)
 
     var log = logger();
 
-    // Register app.locals (app.helper):
-    logger.registerAppHelper(app);
+    /**
+     * Settings
+     */
+    (function(app) {
+        var env = process.env.NODE_ENV || 'development';
+        if ('production' === env) {
+            app.use(express.errorHandler());
+            // a custom "verbose errors" setting which can be used in the templates via settings['verbose errors']
+            app.disable('verbose errors'); // disable verbose errors in production
+            app.enable('view cache'); // Enables view template compilation caching in production
+        }
+        if ('development' === env) {
+            app.use(express.errorHandler({
+                dumpExceptions: true,
+                showStack: true
+            }));
+            // a custom "verbose errors" setting which can be used in the templates via settings['verbose errors']
+            app.enable('verbose errors'); // enable verbose errors in development
+            app.disable('view cache'); // Disables view template compilation caching in development
+        }
 
-    // Define view engine with its options
-    for (var i = 0; i < settings.view.engines.length; ++i) {
-        var extension = settings.view.engines[i].extension;
-        var template = settings.view.engines[i].template;
-        app.engine(extension, engines[template]);
-    }
-    app.set('view engine', settings.view.defaultExtension);
-    app.set('views', path.join(__dirname, 'views'));
+        app.enable('trust proxy'); // Enables reverse proxy support, disabled by default
+        app.enable('case sensitive routing'); // Enable case sensitivity, disabled by default, treating "/Foo" and "/foo"as the same
+        app.enable('strict routing'); // Enable strict routing, by default "/foo" and "/foo/" are treated the same by the router
 
-    // Enables reverse proxy support
-    app.enable('trust proxy');
-    app.enable('jsonp callback');
+        for (var i = 0; i < settings.view.engines.length; ++i) {
+            var extension = settings.view.engines[i].extension;
+            var template = settings.view.engines[i].template;
+            app.engine(extension, engines[template]);
+        }
+        app.set('view engine', settings.view.defaultExtension); // The default engine extension to use when omitted
+        app.set('views', path.join(__dirname, 'views')); // The view directory path
+
+        log.debug('app.settings: %j', app.settings);
+    }(app));
 
     /**
-     * Auto-load bundled middleware
+     * Multi-host
      */
-    var middleware = {};
+    (function(app) {
+        for (var i = 0; i < settings.multihost.index.length; ++i) {
+            var options = settings.multihost.index[i];
 
-    fs.readdirSync(__dirname + '/lib/middleware').forEach(function(filename) {
-        if ( ! /\.js$/.test(filename)) {
-            return;
+            /**
+             * Modules are cached after the first time they are loaded.
+             * The cached module must be invalidated to ensure data-independence
+             * in a multi-host environment.
+             */
+            var server_path = path.join(settings.multihost.path, options.server);
+            if (require.cache[server_path]) {
+                delete require.cache[server_path];
+            }
+            var server = require(server_path);
+            if ( ! server) {
+                log.error('The multi-host server does not exist: %s', server_path);
+                return;
+            }
+
+            app.use(middleware.multihost({
+                hosts: options.hosts,
+                route: options.route,
+                server: server(options.hosts, options.route)
+            }));
+
+            log.info('Attached a multi-host server: %j', options);
         }
-        var name = path.basename(filename, '.js');
-        middleware[name] = require('./lib/middleware/' + name);
-    });
-
-    /**
-     * Multihost
-     */
-    for (var i = 0; i < settings.multihost.index.length; ++i) {
-        var options = settings.multihost.index[i];
-
-        /**
-         * Modules are cached after the first time they are loaded.
-         * The cached module must be invalidated to ensure data-independence
-         * in a multi-host environment.
-         */
-        var server_path = path.join(settings.multihost.path, options.server);
-        if (require.cache[server_path]) {
-            delete require.cache[server_path];
-        }
-        var server = require(server_path);
-        if ( ! server) {
-            log.error('The multi-host server does not exist: %s', server_path);
-            return;
-        }
-
-        app.use(middleware.multihost({
-            hosts: options.hosts,
-            route: options.route,
-            server: server(options.hosts, options.route)
-        }));
-
-        log.info('Attached a multi-host server: %j', options);
-    }
+    }(app));
 
     // Log errors
     app.use(function(err, req, res, next) {
